6880fb347036f2fe7d694c9a022846968f351613
Jeth0214 <rolandjethrosuyom@yahoo.com>
2020-08-14 18:02:29 +0300

    change random quote api

--- qoute.js
+++ qoute.js
@@ -1,57 +1,53 @@
-const RANDOM_QOUTE_API_URL = 'http://api.quotable.io/random';
-const author = document.getElementById('author');
-const text = document.getElementById('text');
-let randomFontColor = document.getElementsByClassName('randomColor');
-let bgColor = document.getElementsByClassName('bgRandomColor');
+const RANDOM_QOUTE_API_URL = "https://thesimpsonsquoteapi.glitch.me/quotes";
+const author = document.getElementById("author");
+const text = document.getElementById("text");
+let randomFontColor = document.getElementsByClassName("randomColor");
+let bgColor = document.getElementsByClassName("bgRandomColor");
 
-document.getElementById('new-quote').addEventListener("click", getRandomQoute);
+document.getElementById("new-quote").addEventListener("click", getRandomQoute);
 
-
-
-// fetch data from api and render it to the page 
+// fetch data from api and render it to the page
 // everytime the page is load and the "new qoute" button is clicked
-function getRandomQoute(){
-    fetch('https://api.quotable.io/random')
-    .then(response => response.json())
-    .then(data => {
-        text.innerHTML = `<span class="quotation-mark">"</span> ${data.content}  `;
-      author.textContent = `- ${data.author}`; 
+function getRandomQoute() {
+  fetch(RANDOM_QOUTE_API_URL)
+    .then((response) => response.json())
+    .then((data) => {
+      text.innerHTML = `<span class="quotation-mark">"</span> ${data[0].quote}  `;
+      author.textContent = `- ${data[0].character}`;
+      console.log(data);
     })
+    .catch(function (err) {
+      console.log(err);
+    });
 
-  changeColor()
-};
-
+  changeColor();
+}
 
 //change the background and font color
 //everytime the page load and the "New Quote" button was clicked
-function changeColor(){
+function changeColor() {
   color = createColor();
-  for(let i = 0; i < randomFontColor.length; i++){
-    
-     randomFontColor[i].style.color = color;
+  for (let i = 0; i < randomFontColor.length; i++) {
+    randomFontColor[i].style.color = color;
   }
-  for(let i = 0; i < bgColor.length; i++){
+  for (let i = 0; i < bgColor.length; i++) {
     bgColor[i].style.backgroundColor = color;
-    
-   }
-};
-
-
+  }
+}
 
 // a function to generate random color for the
 // background and font
-function createColor(){
-     //pick a "red" from 0 - 255
-     var r = Math.floor(Math.random() * 255);
-     //pick a "green" from 0 - 255
-     var g = Math.floor(Math.random() * 255);
-     //pick a "blue" from 0 - 255
-     var b = Math.floor(Math.random() * 255);
- 
-     return "rgb(" + r + ", " + g + ", " + b + ")";
+function createColor() {
+  //pick a "red" from 0 - 255
+  var r = Math.floor(Math.random() * 255);
+  //pick a "green" from 0 - 255
+  var g = Math.floor(Math.random() * 255);
+  //pick a "blue" from 0 - 255
+  var b = Math.floor(Math.random() * 255);
+
+  return "rgb(" + r + ", " + g + ", " + b + ")";
 }
 
-
+// call the funtion everytime the page load
 getRandomQoute();
 changeColor();
-
